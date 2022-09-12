76f82edcae0d1ede8be5df75c106b175bd660606
unknown <stevenchrist123@gmail.com>
2022-09-12 13:59:06 +0700

    tambah semua

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from katalog.models import CatalogItem
+data_barang_katalog = CatalogItem.objects.all()
+context = {
+    'list_barang': data_barang_katalog,
+    'nama': 'Nyoo Steven',
+    'npm': '2106630050',
+}
 # TODO: Create your views here.
+def show_katalog(request):
+    
+    return render(request, "katalog.html",context)
